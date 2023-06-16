fe5787b70f42138fd80935490aea619972046d8f
Adam O'Dwyer <adam@betconnect.com>
2023-06-16 21:26:40 +0100

    tweak layout

--- resources/views/_errors.blade.php
+++ resources/views/_errors.blade.php
@@ -1,8 +1,3 @@
 <div class="alert alert-danger mt-3" role="alert">
-    <i class="material-icons" style="display: inline-flex; vertical-align: top;">error</i> There are errors:
-    <ul class="mt-2">
-    @foreach($errors->all() as $err)
-        <li>{{ $err }}</li>
-    @endforeach
-    </ul>
+    <i class="material-icons" style="display: inline-flex; vertical-align: top;">error</i> Error: please click a location on the map.
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,40 +12,50 @@
     <div id="map" style="height:500px; width: 100%;" class="my-3"></div>
 
     <div class="container">
-        <div class="alert alert-info">
-            <h2><i class="material-icons">info_outline</i> Instructions</h2>
-            <ul>
-                <li>Click a location on the map or drag the current marker to change the location.</li>
-                <li>Click the "Add Point of Interest" button to save a point of interest to the database.</li>
-            </ul>
-        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="alert alert-info">
+                    <h2><i class="material-icons">info_outline</i> Instructions</h2>
+                    <ul>
+                        <li>Click a location on the map or drag the current flag marker to change the location.</li>
+                        <li>Click the "Add Point of Interest" button to save a point of interest to the database.</li>
+                    </ul>
+                </div>
+            </div> <!-- col-md-6 -->
 
-        <div class="form-inline">
-            <div class="form-group mb-2">
-                <label for="name" class="sr-only">Name</label>
-                <input type="text" class="form-control" placeholder="name" name="name" id="name">
+            <div class="col-md-6">
+
+            <div class="form-inline">
+                <div class="form-group mb-2">
+                    <label for="name" class="sr-only">Name</label>
+                    <input type="text" class="form-control mr-2" placeholder="name (optional)" name="name" id="name">
+                </div>
+                <button type="submit" class="btn btn-primary mb-2">Add Point of Interest</button>
             </div>
-            <button type="submit" class="btn btn-primary mb-2">Add Point of Interest</button>
-        </div>
 
-        @includeWhen($errors->any(), '_errors')
+            @includeWhen($errors->any(), '_errors')
 
-        @if (session('success'))
+            @if (session('success'))
             <div class="alert alert-success mt-3">
                 <i class="material-icons" style="display: inline-flex; vertical-align: top;">check</i> {{ session('success') }}
             </div>
-        @endif
+            @endif
 
-        <div class="card mt-4">
-            <div class="card-header">Existing Points of Interest</div>
-            <ul class="list-group list-group-flush">
-            @forelse($locations as $location)
-                <li class="list-group-item">{{ $location->name }} - {{ $location->latitude }}, {{ $location->longitude }}</li>
-            @empty
-                <li class="list-group-item text-muted">No points of interest recorded yet.</li>
-            @endforelse
-            </ul>
-        </div>
+            <div class="card mt-4">
+                <div class="card-header">Existing Points of Interest</div>
+                <ul class="list-group list-group-flush">
+                @forelse($locations as $location)
+                    <li class="list-group-item">
+                        {{ $location->name }} - {{ $location->latitude }}, {{ $location->longitude }} 
+                        <span class="text-muted ml-2">(Created: {{ $location->created_at }})</span>
+                    </li>
+                @empty
+                    <li class="list-group-item text-muted">No points of interest recorded yet.</li>
+                @endforelse
+                </ul>
+            </div>
+
+        </div> <!-- col-md-6 -->     
 
     </div>
 </form>
